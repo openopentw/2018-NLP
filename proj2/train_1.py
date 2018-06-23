881be5b89f8a56c118c5ac1c9c13d9fd64c4b025
YJC <openopentw@gmail.com>
2018-06-24 02:22:22 +0800

    maybe work, but it takes too long time

--- proj2/train_1.py
+++ proj2/train_1.py
@@ -5,13 +5,25 @@ import sys #argv
 import nltk
 import numpy as np
 import pandas as pd
-# import xgboost as xgb
+import xgboost as xgb
 
 from gensim.models import KeyedVectors
+from sklearn import metrics
 
 import bt2us
 
 MISSING_VAL = -999
+
+XGB_PARAMS = {
+    'eta': 0.05,
+    'max_depth': 5,
+    'subsample': 0.6,
+    'colsample_bytree': 1,
+    'objective': 'multi:softmax',
+    'num_class': 19,
+    'eval_metric': 'mlogloss',
+    'silent': 0
+}
 
 RELS = {
     'cause-effect': 0,
@@ -42,6 +54,25 @@ def get_entity(sen):
     e_2 = bt2us.trans_term(e_2)
     return e_1, e_2
 
+def get_rel(rel_str):
+    """ Get the relation labeled in the given string.
+
+    Args:
+        rel_str: the string that contains the string that identify the
+            relation.
+
+    Return: A number that correspond to that relation (specified in
+        dict RELS).
+
+    """
+    rel = rel_str.split('(')[0]
+    order = RELS[rel]
+    if rel != 'other':
+        e_precede = rel_str.split('(')[1].split(',')[0]
+        if e_precede == 'e2':
+            order += 1
+    return order
+
 def read_train(path):
     """ Read training data. """
     with open(path) as infile:
@@ -49,31 +80,21 @@ def read_train(path):
     data = data.lower()
     data = data.split('\n\n')[:-1]
 
-    ret = []
+    x_data = []
+    y_data = []
     for dat in data:
         dat = dat.split('\n')[:2]
         sen = dat[0].split('\t')[1][1:-1]
 
-        # get entities
         e_1, e_2 = get_entity(sen)
+        order = get_rel(dat[1])
 
-        # get relations
-        rel_str = dat[1]
-        rel = rel_str.split('(')[0]
-        order = RELS[rel]
-        if rel != 'other':
-            e_precede = rel_str.split('(')[1].split(',')[0]
-            if e_precede == 'e2':
-                order += 1
-
-        # push_back to ret
-        ret += [{
+        x_data += [{
             'e1': e_1,
             'e2': e_2,
-            # 'rel_str': rel_str,
-            'rel': order,
         }]
-    return ret
+        y_data += [order]
+    return x_data, y_data
 
 def read_test(path):
     """ Read testing data. """
@@ -81,20 +102,36 @@ def read_test(path):
         data = infile.read()
     data = data.lower()
     data = data.split('\n')
+    if data[-1] == '':
+        data = data[:-1]
 
-    ret = []
+    x_data = []
     for dat in data:
         sen = dat.split('\t')[1][1:-1]
 
-        # get entities
         e_1, e_2 = get_entity(sen)
 
-        # push_back to ret
-        ret += [{
+        x_data += [{
             'e1': e_1,
             'e2': e_2,
         }]
-    return ret
+    return x_data
+
+def read_ans(path):
+    """ Read answer keys. """
+    with open(path) as infile:
+        data = infile.read()
+    data = data.lower()
+    data = data.split('\n')
+    if data[-1] == '':
+        data = data[:-1]
+
+    y_data = []
+    for dat in data:
+        rel_str = dat.split('\t')[1]
+        order = get_rel(rel_str)
+        y_data += [order]
+    return y_data
 
 def read_word2vec(path):
     """ Read Google Word2Vec. """
@@ -147,7 +184,7 @@ def term_to_embd(term, dic):
             if token in dic:
                 embds += [dic[token]]
             else:
-                print(token)
+                print('missing: "{}"'.format(token))
                 not_in_cnt += 1
         if embds:
             ret_embd = np.array(embds).mean(axis=0)
@@ -180,27 +217,47 @@ def main():
     """ Main function. """
     assert len(sys.argv) > 1, 'Please give me the path to training file.'
     assert len(sys.argv) > 2, 'Please give me the path to testing file.'
-    assert len(sys.argv) > 3, 'Please give me the path to Google Word2Vec file.'
+    assert len(sys.argv) > 3, 'Please give me the path to answer key file.'
+    assert len(sys.argv) > 4, 'Please give me the path to Google Word2Vec file.'
 
-    train_path = sys.argv[1]
-    test_path = sys.argv[2]
-    w2v_path = sys.argv[3]
-    # sdfin_path = sys.argv[3]
-    # glove_path = sys.argv[3]
-
-    train = read_train(train_path)
-    test = read_train(test_path)
+    x_train, y_train = read_train(sys.argv[1])
+    test = read_test(sys.argv[2])
+    y_test = read_ans(sys.argv[3])
     # json.dump(train[:10], sys.stdout, indent=2)
-    w2v_dic = read_word2vec(w2v_path)
-    # glove_dic = read_glove(glove_path)
-    # sdfin = read_sdfin(sdfin_path, ['token', 'word_vec'])
+    w2v_dic = read_word2vec(sys.argv[4])
+    # glove_dic = read_glove(sys.argv[4])
+    # sdfin = read_sdfin(sys.argv[4], ['token', 'word_vec'])
     # sdfin_dic = sdfin.set_index('token')['word_vec'].to_dict()
-    dic = w2v_dic
 
-    x_train = make_embds(train, dic)
+    dic = w2v_dic
+    x_train = make_embds(x_train, dic)
     x_test = make_embds(test, dic)
     print(x_train[:10])
+    print(x_train.shape)
+    print(x_test[:10])
     print(x_test.shape)
+
+    dtrain = xgb.DMatrix(x_train, y_train)
+    dtest = xgb.DMatrix(x_test)
+
+    cv_output = xgb.cv(XGB_PARAMS,
+                       dtrain,
+                       num_boost_round=2000,
+                       early_stopping_rounds=20,
+                       verbose_eval=25,
+                       show_stdv=True)
+    print('best num_boost_rounds = ', len(cv_output))
+    num_boost_rounds = len(cv_output)
+    # num_boost_rounds = 1436
+    model = xgb.train(XGB_PARAMS,
+                      dtrain,
+                      num_boost_round=num_boost_rounds)
+    pred = model.predict(dtest)
+
+    np.savetxt('a.csv', pred, delimiter=',')
+
+    macro = metrics.f1_score(pred, y_test, average='macro')
+    print('MACRO: {}'.format(macro))
 
 if __name__ == '__main__':
     main()
